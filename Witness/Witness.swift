15b0d8879626eefa3ecce625d24dfd04ff431b2c
Niels de Hoog <njdehoog@gmail.com>
2015-09-23 15:11:08 +0200

    Basic observing

--- Witness/Witness.swift
+++ Witness/Witness.swift
@@ -8,42 +8,29 @@
 
 import Foundation
 
-public class Witness {
-    let paths: [NSString]
-    let changeHandler: () -> ()
-    private var stream: FSEventStreamRef?
+public typealias WitnessEventHandler = () -> ()
+
+public struct Witness {
+    let paths: [String]
+    let changeHandler: WitnessEventHandler
+    private let stream: EventStream
     
-    deinit {
-        if let stream = stream {
-            // stop stream
-            FSEventStreamStop(stream)
-            
-            // unschedule from all run loops
-            FSEventStreamInvalidate(stream)
-            
-            // release
-            FSEventStreamRelease(stream)
-        }
-    }
-    
-    public init(paths: [NSString], changeHandler: () -> ()) {
+    public init(paths: [String], changeHandler: WitnessEventHandler) {
         self.paths = paths
         self.changeHandler = changeHandler
+        
+        self.stream = EventStream(paths: paths, changeHandler: changeHandler)
     }
     
-    public func start() {
-        func callBack (stream: ConstFSEventStreamRef, clientCallbackInfo: UnsafeMutablePointer<Void>, numEvents: Int, eventPaths: UnsafeMutablePointer<Void>, eventFlags: UnsafePointer<FSEventStreamEventFlags>, eventIDs: UnsafePointer<FSEventStreamEventId>) -> Void {
-            let witness: Witness = unsafeBitCast(clientCallbackInfo, Witness.self)
-            witness.changeHandler()
-        }
+    public init(paths: [String], streamType: StreamType, deviceToWatch: dev_t,  changeHandler: WitnessEventHandler) {
+        self.paths = paths
+        self.changeHandler = changeHandler
         
-        var context = FSEventStreamContext()
-        context.info = unsafeBitCast(self, UnsafeMutablePointer<Void>.self)
-        
-        let stream = FSEventStreamCreate(nil, callBack, &context, paths, FSEventStreamEventId(kFSEventStreamEventIdSinceNow), 1.0, FSEventStreamCreateFlags(kFSEventStreamCreateFlagNone))
-        FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode)
-        FSEventStreamStart(stream)
-        self.stream = stream
+        self.stream = EventStream(paths: paths, type: streamType, deviceToWatch: deviceToWatch, changeHandler: changeHandler)
+    }
+    
+    public func flush() {
+        self.stream.flush()
     }
 }
 
@@ -52,16 +39,54 @@ public class Witness {
 */
 
 public enum StreamType {
-    case HostBased
+    case HostBased // default
     case DiskBased
 }
 
-class CallbackProxy {
-    func helloWorld() {
-        print("Hello world. This is a proxy object")
+private class EventStream {
+    // use explicitly unwrapped optional so we can pass self as context to stream
+    private var stream: FSEventStreamRef!
+    private let changeHandler: WitnessEventHandler
+    static let latency = 1.0
+    
+    init(paths: [String], type: StreamType = .HostBased, deviceToWatch: dev_t = 0, changeHandler: WitnessEventHandler) {
+        self.changeHandler = changeHandler
+        
+        func callBack (stream: ConstFSEventStreamRef, clientCallbackInfo: UnsafeMutablePointer<Void>, numEvents: Int, eventPaths: UnsafeMutablePointer<Void>, eventFlags: UnsafePointer<FSEventStreamEventFlags>, eventIDs: UnsafePointer<FSEventStreamEventId>) -> Void {
+            let eventStream = unsafeBitCast(clientCallbackInfo, EventStream.self)
+            eventStream.changeHandler()
+        }
+        
+        var context = FSEventStreamContext()
+        context.info = unsafeBitCast(self, UnsafeMutablePointer<Void>.self)
+        
+        switch type {
+        case .HostBased:
+            stream = FSEventStreamCreate(nil, callBack, &context, paths, FSEventStreamEventId(kFSEventStreamEventIdSinceNow), EventStream.latency, FSEventStreamCreateFlags(kFSEventStreamCreateFlagNone))
+        case .DiskBased:
+            stream = FSEventStreamCreateRelativeToDevice(nil, callBack, &context, deviceToWatch, paths, FSEventStreamEventId(kFSEventStreamEventIdSinceNow), EventStream.latency, FSEventStreamCreateFlags(kFSEventStreamCreateFlagNone))
+        }
+        
+        FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode)
+        FSEventStreamStart(stream)
+    }
+    
+    func flush() {
+        FSEventStreamFlushSync(stream)
+    }
+    
+    deinit {
+        // stop stream
+        FSEventStreamStop(stream)
+        
+        // unschedule from all run loops
+        FSEventStreamInvalidate(stream)
+        
+        // release
+        FSEventStreamRelease(stream)
     }
 }
 
-class EventStream {
-    
-}
+
+
+
--- WitnessTests/WitnessTests.swift
+++ WitnessTests/WitnessTests.swift
@@ -12,37 +12,68 @@ import XCTest
 class WitnessTests: XCTestCase {
     let fileManager = NSFileManager()
     var witness: Witness?
-    let timeout = 3.0
+    let timeout = 2.0
     
-    var desktopPath: String {
-        // TODO: change to some kind of temp directory
-        return ("~/Desktop" as NSString).stringByExpandingTildeInPath
+    var temporaryDirectory: String {
+        return NSTemporaryDirectory()
+    }
+    
+    var testsDirectory: String {
+        return (temporaryDirectory as NSString).stringByAppendingPathComponent("WitnessTests")
     }
     
     var filePath: String {
-        return (desktopPath as NSString).stringByAppendingPathComponent("file.temp")
+        return (testsDirectory as NSString).stringByAppendingPathComponent("file.temp")
     }
     
     override func setUp() {
         super.setUp()
+        
+        // create tests directory
+        try! fileManager.createDirectoryAtPath(testsDirectory, withIntermediateDirectories: true, attributes: nil)
     }
     
     override func tearDown() {
-        try! self.fileManager.removeItemAtPath(filePath)
+        witness?.flush()
+        witness = nil
+        
+        do {
+            // remove tests directory
+            try fileManager.removeItemAtPath(testsDirectory)
+        }
+        catch {}
+        
         super.tearDown()
     }
     
-    func testCreateWitness() {
-        let expectation = expectationWithDescription("Directory change")
-        
-        witness = Witness(paths: [desktopPath]) {
-            print("directory changed")
+    func testThatFileCreationIsObserved() {
+        let expectation = expectationWithDescription("File creation")
+        witness = Witness(paths: [testsDirectory]) {
             expectation.fulfill()
         }
-        witness?.start()
-        
-        try! "".writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding)
-        
+        fileManager.createFileAtPath(filePath, contents: nil, attributes: nil)
+        waitForExpectationsWithTimeout(timeout, handler: nil)
+    }
+    
+    func testThatFileRemovalIsObserved() {
+        let expectation = expectationWithDescription("File removal")
+        fileManager.createFileAtPath(filePath, contents: nil, attributes: nil)
+        sleep(1)
+        witness = Witness(paths: [testsDirectory]) {
+            expectation.fulfill()
+        }
+        try! fileManager.removeItemAtPath(filePath)
+        waitForExpectationsWithTimeout(timeout, handler: nil)
+    }
+    
+    func testThatFileChangesAreObserved() {
+        let expectation = expectationWithDescription("File changes")
+        fileManager.createFileAtPath(filePath, contents: nil, attributes: nil)
+        sleep(1)
+        witness = Witness(paths: [testsDirectory]) {
+            expectation.fulfill()
+        }
+        try! "Hello changes".writeToFile(filePath, atomically: true, encoding: NSUTF8StringEncoding)
         waitForExpectationsWithTimeout(timeout, handler: nil)
     }
 }
