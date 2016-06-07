3ebeae06c6b90221a2495db5c4f8692fb01b3c5a
Niels de Hoog <njdehoog@gmail.com>
2016-06-07 12:23:07 +0200

    Added demo project

---
+++ WitnessDemo/WitnessDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WitnessDemo
+//
+//  Created by Niels de Hoog on 07/06/16.
+//  Copyright © 2016 Invisible Pixel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
