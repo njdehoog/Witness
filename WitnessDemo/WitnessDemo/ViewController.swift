2ef941b1b6694598f3ecc0ee44bef5704460167f
Niels de Hoog <njdehoog@gmail.com>
2016-06-07 12:23:07 +0200

    Added demo project

---
+++ WitnessDemo/WitnessDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WitnessDemo
+//
+//  Created by Niels de Hoog on 07/06/16.
+//  Copyright © 2016 Invisible Pixel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
