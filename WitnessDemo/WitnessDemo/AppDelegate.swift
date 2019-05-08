8005a4f2d7745f8c85c9d80b039bcb61d8c0f929
Matt Long <matt.long@matthew-long.com>
2019-05-08 11:29:22 -0600

    Update the projects to Xcode 10.2 and Swift 5 and make the Witness framework a subproject of the Witness demo and set it up as a dependency

--- WitnessDemo/WitnessDemo/AppDelegate.swift
+++ WitnessDemo/WitnessDemo/AppDelegate.swift
@@ -13,8 +13,8 @@ import Witness
 class AppDelegate: NSObject, NSApplicationDelegate {
     var witness: Witness?
     
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        if let desktopPath = NSSearchPathForDirectoriesInDomains(.DesktopDirectory, .UserDomainMask, true).first {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        if let desktopPath = NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true).first {
             self.witness = Witness(paths: [desktopPath], flags: .FileEvents, latency: 0.3) { events in
                 // create/delete or modify a file on the Desktop to see this event triggered
                 print("file system events received: \(events)")
